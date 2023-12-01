4277dd06e321ba142bb0f0769e3fda799b919e92
Jhomar Izar <jhomar.izar@gmail.com>
2023-12-02 06:03:29 +0800

    Update width and height

--- admin/inventory.php
+++ admin/inventory.php
@@ -1,105 +1,107 @@
 <?php
 include('connection.php');
- include ('header.php');
-    include ('sidebar.php');
- 
+include('header.php');
+include('sidebar.php');
+
 
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $category_name= $_POST['category_name'];
+    $category_name = $_POST['category_name'];
     $med_name = $_POST['med_name'];
     $med_type = $_POST['med_type'];
     $description = $_POST['description'];
     $price = $_POST['price'];
-   
-   
-$check_username_query = "SELECT * FROM med_inventory WHERE med_name='$med_name'";
+
+
+    $check_username_query = "SELECT * FROM med_inventory WHERE med_name='$med_name'";
     $result = $conn->query($check_username_query);
     if ($result->num_rows > 0) {
         echo '<script>alert("Medicine already exists.");</script>';
-    }else{
+    } else {
         $query = "INSERT INTO med_inventory (med_name,category_name,med_type,description,price) VALUES ('$med_name','$category_name','$med_type','$description','$price')";
 
         if ($conn->query($query) === TRUE) {
-            
+
         } else {
             echo '<script>alert("Error: ' . $query . ' ' . $conn->error . '");</script>';
         }
     }
 
-    
-} 
+
+}
 
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Medicine List</title>
-  
-  
-   
+
+
+
     <style>
         .modal {
-      display: none;
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.5);
-      justify-content: center;
-      align-items: center;
-    }
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            justify-content: center;
+            align-items: center;
+        }
 
-    .modal-content {
-      background-color: #fff;
-      padding: 20px;
-      border-radius: 5px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        width: 700px;
-        height: 400px;
-      
-    }
+        .modal-content {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            width: 700px;
+            height: 400px;
 
-    .close {
-      color: #aaa;
-      float: right;
-      font-size: 28px;
-      font-weight: bold;
-      cursor: pointer;
-    }
-    .form-center{
-      margin-top: 10px;
-    }
-    .form-center{
-        margin-top: 60px;
-          margin-left: 570px;
-          width:70%;
+        }
 
-    }
-    th{
-      padding: 10px;
-      text-align: left;
-      margin-left: 10px;
-    }
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+            cursor: pointer;
+        }
 
-        .form-container{
-            width: 25%;
-            margin-top: 5.45%;
-            margin-left: 24%;
+        .form-center {
+            margin-top: 10px;
+        }
+
+        .form-center {
+            margin-top: 60px;
+            margin-left: 570px;
+            width: 70%;
+
+        }
+
+        th {
+            padding: 10px;
+            text-align: left;
+            margin-left: 10px;
+        }
+
+        .form-container {
+            width: auto;
+            margin-top: 60px;
+            margin-left: 245px;
             border-radius: 10px;
             padding: 4px;
-            background-color: #E5E7E9 ;
+            background-color: #E5E7E9;
             float: left;
-
-
-        } 
+        }
 
         .form-container label {
-           
+
             margin-top: 10px;
             text-align: left;
 
@@ -126,9 +128,10 @@ $check_username_query = "SELECT * FROM med_inventory WHERE med_name='$med_name'"
             margin: 0;
             width: 100%;
         }
-        h2.form_name{
+
+        h2.form_name {
             border-radius: 3px;
-            background-color:  #b3ffb3;
+            background-color: #b3ffb3;
             padding-top: 11px;
             text-align: center;
             margin: 0;
@@ -141,14 +144,14 @@ $check_username_query = "SELECT * FROM med_inventory WHERE med_name='$med_name'"
 
 
 
-         table.registered_table {
+        table.registered_table {
             width: 100%;
             border-collapse: collapse;
             margin-top: 20px;
-            margin-left: auto ;
+            margin-left: auto;
             margin-right: auto;
 
-         }
+        }
 
         td {
             border: 1px solid #ddd;
@@ -162,9 +165,9 @@ $check_username_query = "SELECT * FROM med_inventory WHERE med_name='$med_name'"
             text-align: center;
             border: .5px solid dimgray;
 
-         }
+        }
 
-         button {
+        button {
             cursor: pointer;
             padding: 8px 12px;
             border: none;
@@ -180,34 +183,38 @@ $check_username_query = "SELECT * FROM med_inventory WHERE med_name='$med_name'"
             background-color: red;
             color: white;
         }
-        h2.user_list{
+
+        h2.user_list {
             background-color: #b3ffb3;
             width: 100%;
             margin: 0;
             padding: 15px;
             margin-bottom: 15px;
-          
+
             border-bottom: 1px solid black;
         }
 
-    input.num{
-        padding: 3px;
-        width: 200px;
-    }
-    .des, .pr{
-        width: 70%;
-        margin-left: 15%;
-     }
-    .pr{
-        padding: 3.5px;
-    }
+        input.num {
+            padding: 3px;
+            width: 200px;
+        }
 
+        .des,
+        .pr {
+            width: 70%;
+            margin-left: 15%;
+        }
+
+        .pr {
+            padding: 3.5px;
+        }
     </style>
 </head>
+
 <body>
-    
+
     <?php
-        $query = "SELECT mi.id, m.med_name as med_name, mc.category_name as category_name, mt.med_type as med_type, mi.description, mi.price 
+    $query = "SELECT mi.id, m.med_name as med_name, mc.category_name as category_name, mt.med_type as med_type, mi.description, mi.price 
           FROM med_inventory mi
           JOIN medicines m ON mi.med_name = m.id
           JOIN med_category mc ON mi.category_name = mc.id
@@ -220,61 +227,69 @@ $check_username_query = "SELECT * FROM med_inventory WHERE med_name='$med_name'"
         <h2 class="form_name">PRODUCT FORM</h2>
         <form style="margin-top: 10px;" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 
-    <label for="med_name">Medicine name:</label>
-         <select name="med_name">
-            <?php 
+            <label for="med_name">Medicine name:</label>
+            <select name="med_name">
+                <?php
                 $medicines = mysqli_query($conn, "SELECT * FROM medicines");
-                while($c = mysqli_fetch_array($medicines)){
-            ?>
-            <option value="<?php echo $c['med_name']?>"><?php echo $c['med_name'] ?></option>
-            <?php } ?>
-        </select>
-    <br>
-    <label for="category_name">Category:&nbsp;</label><br>
-        <select name="category_name">
-            <?php 
+                while ($c = mysqli_fetch_array($medicines)) {
+                    ?>
+                    <option value="<?php echo $c['med_name'] ?>">
+                        <?php echo $c['med_name'] ?>
+                    </option>
+                <?php } ?>
+            </select>
+            <br>
+            <label for="category_name">Category:&nbsp;</label><br>
+            <select name="category_name">
+                <?php
                 $med_category = mysqli_query($conn, "SELECT * FROM med_category");
-                while($c = mysqli_fetch_array($med_category)){
-            ?>
-            <option value="<?php echo $c['category_name']?>"><?php echo $c['category_name'] ?></option>
-            <?php } ?>
-        </select>
-    <br><br>
+                while ($c = mysqli_fetch_array($med_category)) {
+                    ?>
+                    <option value="<?php echo $c['category_name'] ?>">
+                        <?php echo $c['category_name'] ?>
+                    </option>
+                <?php } ?>
+            </select>
+            <br><br>
 
-    <label for="med_type">Type:</label>
-     <select name="med_type">
-            <?php 
+            <label for="med_type">Type:</label>
+            <select name="med_type">
+                <?php
                 $med_type = mysqli_query($conn, "SELECT * FROM med_type");
-                while($c = mysqli_fetch_array($med_type)){
-            ?>
-            <option value="<?php echo $c['med_type']?>"><?php echo $c['med_type'] ?></option>
-            <?php } ?>
-        </select>
+                while ($c = mysqli_fetch_array($med_type)) {
+                    ?>
+                    <option value="<?php echo $c['med_type'] ?>">
+                        <?php echo $c['med_type'] ?>
+                    </option>
+                <?php } ?>
+            </select>
 
-    <label for="description"><br>Description:</label><br>
-    <textarea class="des" id="description" name="description" rows="4" placeholder="Enter your description here..."></textarea>
-    <br>
+            <label for="description"><br>Description:</label><br>
+            <textarea class="des" id="description" name="description" rows="4"
+                placeholder="Enter your description here..."></textarea>
+            <br>
 
-    <label for="price"><br>Product Price:</label><br>
-    <input class="pr" type="price" id="price" name="price" required>
+            <label for="price"><br>Product Price:</label><br>
+            <input class="pr" type="price" id="price" name="price" required>
 
-    <br><br><br>
-    <button style="margin-top: px; width:100%; background-color: lightblue;" type="submit" value="Save">Save</button>
-        
-</form>
+            <br><br><br>
+            <button style="margin-top: px; width:100%; background-color: lightblue;" type="submit"
+                value="Save">Save</button>
+
+        </form>
 
     </div>
 
-<div class="form-center">
-           
-    <?php
+    <div class="form-center">
+
+        <?php
         $query = "SELECT * FROM med_inventory";
         $result = $conn->query($query);
-    ?>
-    <h2 class="user_list">PRODUCT LIST</h2>
-    
-       
-        <table  class="registered_table">
+        ?>
+        <h2 class="user_list">PRODUCT LIST</h2>
+
+
+        <table class="registered_table">
             <tr class="registered_tr">
                 <th>Id</th>
                 <th>Name</th>
@@ -284,39 +299,40 @@ $check_username_query = "SELECT * FROM med_inventory WHERE med_name='$med_name'"
                 <th>Price</th>
                 <th>Action</th> <!-- New column for buttons -->
             </tr>
-    
-    <?php
-    // Loop through the rows in the result set
-    while ($row = $result->fetch_assoc()) {
-        echo '<tr>';
-        echo '<td>' . $row['id']. '</td>';
-        echo '<td>' . $row['med_name'] . '</td>';
-        echo '<td>' . $row['category_name'] . '</td>';
-        echo '<td>' . $row['med_type'] . '</td>';
-        echo '<td>' . $row['description'] . '</td>';
-        echo '<td>' . $row['price'] . '</td>';
-        echo '<td>';
-        echo '<button style="background-color: green; color: white; padding: 5px 10px;">Edit</button>'; // Green Edit button
-        echo ' ';
-        echo '<button style="background-color: red; color: white; padding: 5px 10px;">Delete</button>'; // Red Delete button
-        echo '</td>';
-        echo '</tr>';
-    }
-    ?>
-</table>
-<br>
-<br>
+
+            <?php
+            // Loop through the rows in the result set
+            while ($row = $result->fetch_assoc()) {
+                echo '<tr>';
+                echo '<td>' . $row['id'] . '</td>';
+                echo '<td>' . $row['med_name'] . '</td>';
+                echo '<td>' . $row['category_name'] . '</td>';
+                echo '<td>' . $row['med_type'] . '</td>';
+                echo '<td>' . $row['description'] . '</td>';
+                echo '<td>' . $row['price'] . '</td>';
+                echo '<td>';
+                echo '<button style="background-color: green; color: white; padding: 5px 10px;">Edit</button>'; // Green Edit button
+                echo ' ';
+                echo '<button style="background-color: red; color: white; padding: 5px 10px;">Delete</button>'; // Red Delete button
+                echo '</td>';
+                echo '</tr>';
+            }
+            ?>
+        </table>
+        <br>
+        <br>
 
 
-</div>
+    </div>
 
     <?php
     $conn->close();
     ?>
-        
+
     </div>
 
-    
-    
+
+
 </body>
+
 </html>
--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,16 +1,16 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <style>
-        body{
-        margin: 0;
-        padding: 0;
-        position: fixed;
-        height: auto;
-        width: 81.5%;
-        
-    }
+        body {
+            margin: 0;
+            padding: 0;
+            position: fixed;
+            height: auto;
+            width: 81.5%;
+
+        }
 
         .navbar {
             position: fixed;
@@ -20,8 +20,8 @@
             color: #fff;
             padding: 10px 20px;
             align-items: center;
-            width: 1325px;
-
+            width: 100%;
+            z-index: 999;
 
         }
 
@@ -43,71 +43,76 @@
             padding: 5px 10px;
             cursor: pointer;
             text-decoration: none;
-            margin-left: 10px; /* Add margin to separate the buttons */
+            margin-right: 50px;
+            /* Add margin to separate the buttons */
             transition: .1s;
         }
 
         .nav-btn:hover {
             background-color: #007bff;
             color: #fff;
-            
+
         }
 
 
 
         .sidebar {
-    
-    margin-top: 4.2%;
-    background-color: #333; /* Sidebar background color */
-    color: #fff; /* Text color */
-    width: 200px; /* Sidebar width */
-    display: block;
-    flex-direction: column;
-    height: 700px;
-    padding: 20px;
-    position: fixed;
-}
+            top:0;
+            background-color: #333;
+            /* Sidebar background color */
+            color: #fff;
+            /* Text color */
+            width: 200px;
+            /* Sidebar width */
+            display: block;
+            flex-direction: column;
+            height: 100%;
+            padding: 20px;
+            position: fixed;
+        }
 
-.sidebar ul {
-    list-style: none;
-    padding: 20px;
-}
+        .sidebar ul {
+            list-style: none;
+            padding: 20px;
+            margin-top:50px;
+        }
 
-.sidebar ul li {
-    margin-bottom: 40px;
-}
+        .sidebar ul li {
+            margin-bottom: 40px;
+        }
 
-.sidebar a {
-    text-decoration: none;
-    color: #fff; /* Link text color */
-    display: block;
-    font-size: 16px;
-    transition: .7s;
+        .sidebar a {
+            text-decoration: none;
+            color: #fff;
+            /* Link text color */
+            display: block;
+            font-size: 16px;
+            transition: .7s;
 
-}
+        }
 
-.sidebar a:hover {
-    background-color: #007bff; /* Hover background color */
-    color: #fff; /* Hover text color */
-    border-radius: 3px;
-    padding: 5px 10px;
+        .sidebar a:hover {
+            background-color: #007bff;
+            /* Hover background color */
+            color: #fff;
+            /* Hover text color */
+            border-radius: 3px;
+            padding: 5px 10px;
 
-}
-div.content-section{
-    display: inline-block;
-    
+        }
 
+        div.content-section {
+            display: inline-block;
 
-
-
-        
+        }
     </style>
 </head>
+
 <body>
     <div class="navbar">
         <h1 onclick="reloadPage()">Philzen Drugstore</h1>
         <div class="nav-btn-container">
-           
+
             <a href="../index.php" class="nav-btn">Logout</a>
         </div>
     </div>
@@ -118,24 +123,25 @@ div.content-section{
         }
     </script>
 
-     
 
-    	 <div class="sidebar">
+
+    <div class="sidebar">
         <div class="header">
-            
+
             <!-- Add your header content here -->
         </div>
         <ul>
-            <li><a href="">Dashboard</a></li>
+            <li><a href="dashbrd.php">Dashboard</a></li>
             <li><a href="inventory.php">Inventory</a></li>
             <li><a href="med_nventory.php">Medicine List</a></li>
             <li><a href="med_category.php">Medicine Category</a></li>
             <li><a href="med_type.php">Medicine Type</a></li>
-            <li><a href="med_supplier.php">Supplier</a></li>   
+            <li><a href="med_supplier.php">Supplier</a></li>
             <li><a href="phar_registration.php">Registration</a></li>
         </ul>
     </div>
 
 
 </body>
+
 </html>
